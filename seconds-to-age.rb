e1f3d2fc33bcb8703faeef29bbb46106081da8d5
Pooya Tolideh <pooya.tolideh@gmail.com>
2016-10-09 17:59:35 -0400

    Edit comments

--- seconds-to-age.rb
+++ seconds-to-age.rb
@@ -1,13 +1,16 @@
-# This function gets your age in total seconds and returns how old you are actually
-# assumtion_1: is that each year is 365.25 days belongs_to
+# This program gets your age in total seconds and returns how old you actually are are
+# example: 1246000000 => 39yr, 5mth, 24d, 0hr, 0min, 40sec
+
+# assumtion_1: Each year is 365.25 days
 # assumption_2: Months have even number of days
 
-#calculate the actual number
+
+# returns the main division value in int
 def get_num(a,b)
 	return (a/b).to_i
 end
-fs
-#calculate the remainder
+
+# returns a remainder in int
 def get_rm (a,b)
 	return (a%b).to_i
 end
@@ -18,7 +21,7 @@ def age(total)
 	mth_sec = (365.25/12)*24*3600
 	day_sec = 24*3600
 	
-	#remainders after divisions
+	#remainders of divisions
 	yr_rm = get_rm(total, yr_sec)
 	mth_rm = get_rm(yr_rm,mth_sec)
 	day_rm = get_rm(mth_rm,3600)
