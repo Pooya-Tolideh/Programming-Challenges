0a527b55a5de62f256c4c32d3fca31932cdbe218
Pooya Tolideh <pooya.tolideh@gmail.com>
2016-10-19 01:21:35 -0400

    feat: Get contact information from user
    Get first name, last name and phone number and place it in 'first_name', 'last_name', and 'phone'

---
+++ phone-book.rb
@@ -0,0 +1,10 @@
+puts "Enter your contacts' information"
+
+print "\nFirst Name: "
+first_name = gets.chomp.downcase.capitalize
+
+print "\nLast Name: "
+last_name = gets.chomp.downcase.capitalize
+
+print "\nPhone Number: "
+phone = gets.chomp
