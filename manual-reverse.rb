d40c16e526d24bc9dbafbc59d523491d5c216a89
Pooya Tolideh <pooya.tolideh@gmail.com>
2016-10-18 22:44:09 -0400

    FEAT: Add start( ) to get an array from user
    This function asks users to enter as many items as they like. Then places those items in an array.

--- manual-reverse.rb
+++ manual-reverse.rb
@@ -1,15 +1,33 @@
 # This is an algorithm for reversing an array without using 'reverse' method
 
 
-arr = ['dog', 'cat', 'bear','hello']
-arr_ln = arr.size - 1
+# arr = ['dog', 'cat', 'bear','hello']
+# arr_ln = arr.size - 1
 
-# using a temporary array
-arr_rev << arr.pop until arr.empty?
+# # using a temporary array
+# arr_rev << arr.pop until arr.empty?
 
-# not using a temporary array
-(0..(arr_ln/2)).each do |i|
-	x = arr[i]
-	arr[i] =  arr[(arr_ln - i)]
-	arr[(arr_ln - i)] = x
+# # not using a temporary array
+# (0..(arr_ln/2)).each do |i|
+# 	x = arr[i]
+# 	arr[i] =  arr[(arr_ln - i)]
+# 	arr[(arr_ln - i)] = x
+# end
+
+def start()
+
+	puts "Give me some stuff to put in an array. Say \"done\" when your array is finished"
+	
+	arr = []
+	while true 
+		print "> "
+		item = $stdin.gets.chomp
+		if item.downcase == "done"
+			break
+		else
+			arr << item
+		end
+	end
 end
+
+start
