79449314836c849a095201d41a8919d954197e5d
Pooya Tolideh <pooya.tolideh@gmail.com>
2016-10-24 20:27:54 -0400

    refactor: Simplify contacts list formation process

--- phone-book.rb
+++ phone-book.rb
@@ -1,5 +1,4 @@
 puts "Enter your contacts' information"
-contact_array = []
 
 print "\nFirst Name: "
 first_name = gets.chomp.downcase.capitalize
@@ -10,8 +9,5 @@ last_name = gets.chomp.downcase.capitalize
 print "\nPhone Number: "
 phone = gets.chomp
 
-person_info = []
-person_info << first_name + " " + last_name
-person_info << phone
-
-contact_array << person_info
+person_info = Hash.new
+contacts[(first_name+" "+last_name)] = phone
