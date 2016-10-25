82539df79ee43ba676408e6336bf6d22f0851eb3
Pooya Tolideh <pooya.tolideh@gmail.com>
2016-10-24 20:58:43 -0400

    feat: Add while-loops and add_more to get multiple inputs

--- phone-book.rb
+++ phone-book.rb
@@ -1,14 +1,34 @@
 puts "Enter your contacts' information"
-
-print "\nFirst Name: "
-first_name = gets.chomp.downcase.capitalize
-
-print "\nLast Name: "
-last_name = gets.chomp.downcase.capitalize
-
-while true
-	print "\nPhone Number: "
-	phone = gets.chomp
-	(phone.to_i == 0) ? (puts "\n\nInvalid input. Please enter a valid number!") : break
-end 
+finish = false
 contacts = Hash.new
+
+while !finish 
+	print "\nFirst Name: "
+	first_name = gets.chomp.downcase.capitalize
+
+	print "\nLast Name: "
+	last_name = gets.chomp.downcase.capitalize
+
+	while true
+		print "\nPhone Number: "
+		phone = gets.chomp
+		(phone.to_i == 0) ? (puts "\n\nInvalid input. Please enter a valid number!") : break
+	end 
+
+	contacts[(first_name+" "+last_name)] = phone
+	
+	while true 
+		puts "-"*10
+		print "\nAdd another contanct? [ y/n ]: "
+		add_more = gets.chomp.downcase
+		if add_more == 'n'
+			finish = true
+			break
+		elsif add_more == 'y'
+			puts ">>>"
+			break
+		else 
+			puts "\nI did not understand that!\n"
+		end
+	end
+end
