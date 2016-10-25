d77153d04f6475e0970daa8ec27952f330edfeb5
Pooya Tolideh <pooya.tolideh@gmail.com>
2016-10-24 21:05:50 -0400

    feat: Alert if both first name or last name field empty

--- phone-book.rb
+++ phone-book.rb
@@ -8,27 +8,30 @@ while !finish
 
 	print "\nLast Name: "
 	last_name = gets.chomp.downcase.capitalize
+	if (last_name == '' && first_name == '')
+		puts "At least enter a first name or last name to proceed"
+	else
+		while true
+			print "\nPhone Number: "
+			phone = gets.chomp
+			(phone.to_i == 0) ? (puts "\n\nInvalid input. Please enter a valid number!") : break
+		end 
 
-	while true
-		print "\nPhone Number: "
-		phone = gets.chomp
-		(phone.to_i == 0) ? (puts "\n\nInvalid input. Please enter a valid number!") : break
-	end 
+		contacts[(first_name+" "+last_name)] = phone
 
-	contacts[(first_name+" "+last_name)] = phone
-	
-	while true 
-		puts "-"*10
-		print "\nAdd another contanct? [ y/n ]: "
-		add_more = gets.chomp.downcase
-		if add_more == 'n'
-			finish = true
-			break
-		elsif add_more == 'y'
-			puts ">>>"
-			break
-		else 
-			puts "\nI did not understand that!\n"
+		while true 
+			puts "-"*10
+			print "\nAdd another contanct? [ y/n ]: "
+			add_more = gets.chomp.downcase
+			if add_more == 'n'
+				finish = true
+				break
+			elsif add_more == 'y'
+				puts ">>>"
+				break
+			else 
+				puts "\nI did not understand that!\n"
+			end
 		end
 	end
 end
