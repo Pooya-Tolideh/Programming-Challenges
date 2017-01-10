4d83647c0c0191f195dabd55d1acc33b74b2ed88
Pooya Tolideh <pooya.tolideh@gmail.com>
2017-01-10 02:47:55 -0500

    feat: Add slicer
    gets total number of hours and length of pomodoro and returns the total number of pomodoro

--- Daily-Scheduler/main.py
+++ Daily-Scheduler/main.py
@@ -1,5 +1,12 @@
-def main():	
-	pom = 50.0
+
+
+def slicer(total, pom):									# slices time various portions
+	counter = 1
+	total = round((total * 60)/pom)							# total number of pomodoro sessions
+
+def main():
+	
+	pom = 50.0									# pomodoro duration
 	total = float(input("how much time do you have? "))
   
 main()
