18019eaa396e840f04e653327538227352f7832b
Pooya Tolideh <pooya.tolideh@gmail.com>
2017-01-10 02:52:56 -0500

    Feat: Add Numpy Library and slice times
    divide the total number of Pomodoros into 5 different portions and place them in slices

--- Daily-Scheduler/main.py
+++ Daily-Scheduler/main.py
@@ -1,8 +1,13 @@
+import numpy as np
 
-
-def slicer(total, pom):									# slices time various portions
+def slicer(total, pom):							# slices time various portions
 	counter = 1
-	total = round((total * 60)/pom)							# total number of pomodoro sessions
+	total = round((total * 60)/pom)					# total number of pomodoro sessions
+	
+	slices = [total * 0.4]						#first(most important) portion
+	while (total - np.sum(slices) >= 1 and counter <= 5):		#counter runs until there's no time left or slices reaches total number of projects
+		slices.append((total - np.sum(slices)) / 2)
+		counter += 1
 
 def main():
 	
