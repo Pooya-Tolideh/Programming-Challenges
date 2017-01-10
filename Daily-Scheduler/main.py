b76a1d2e80e28923d1628d77490f0c75ace789fd
Pooya Tolideh <pooya.tolideh@gmail.com>
2017-01-10 00:58:29 -0500

    FEAT: add manage_time function

--- Daily-Scheduler/main.py
+++ Daily-Scheduler/main.py
@@ -2,33 +2,34 @@ def convertor(time):
 	hour = int(time)
 	minute = int((time - hour) * 60)
 	return(hour, minute)
+
+def manage_time():	
+	slices = []
+	total = float(input("how much time do you have? "))
 	
+	slices.append(total * 0.6)
 	
-slices = []
-total = float(input("how much time do you have? "))
-
-slices.append(total * 0.6)
-
-slices.append((total -  slices[0]) * 0.6)
-
-slices.append((total - slices[0] - slices[1]) / 2)
-
-slices.append(slices[-1])
-
-i = 1
-
-for slc in slices:
-	hour, minute = convertor(slc)
-	if hour == 0:
-		hour = ""
-	else:
-		hour = "{}h".format(hour)
-	if minute == 0:
-		minute = ""
-	else:
-		minute = "{}m".format(minute)
-	print("{}. {} {}".format(i, hour, minute))
-	i = i + 1
+	slices.append((total -  slices[0]) * 0.6)
+	
+	slices.append((total - slices[0] - slices[1]) / 2)
+	
+	slices.append(slices[-1])
+	
+	i = 1
+	
+	for slc in slices:
+		hour, minute = convertor(slc)
+		if hour == 0:
+			hour = ""
+		else:
+			hour = "{}h".format(hour)
+		if minute == 0:
+			minute = ""
+		else:
+			minute = "{}m".format(minute)
+		print("{}. {} {}".format(i, hour, minute))
+		i = i + 1
 
 
+manage_time()
 
